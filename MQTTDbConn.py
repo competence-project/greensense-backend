bc2d1b21c5dc185a574b2956fac519d205ded663
MRajczyk <mikolajrajczyk01@gmail.com>
2023-05-19 09:27:08 +0200

    error handling tweaks

--- MQTTDbConn.py
+++ MQTTDbConn.py
@@ -46,7 +46,7 @@ class MQTTDbConn(threading.Thread):
         #topic standard is dev/{mac}/{type}/{id} so after split should be [0]=dev [1]=mac [2]=type [3]=id
         topicTokens = msg.topic.split('/')
         #LE ERRORHANDLINGO
-        if len(topicTokens) != 4:
+        if len(topicTokens) != 4 or topicTokens[2] == "cmd":
             pass
         try:
             self.saveMqttLog(topicTokens[1], topicTokens[2], topicTokens[3], float(util.convertBytesToString(msg.payload)))
