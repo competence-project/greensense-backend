bfc828625389ad3c30cc0a5888aa0fb547824a1d
MRajczyk <mikolajrajczyk01@gmail.com>
2023-06-02 00:48:57 +0200

    reformat

--- MQTT.py
+++ MQTT.py
@@ -27,44 +27,48 @@ if __name__ == "__main__":
     # mqttClient.start()
 
     app = FastAPI()
-    #sample, test endpoint
+
+
+    # sample, test endpoint
     @app.get("/")
     async def root():
         return {"message": "Hello World."}
 
-    #endpoint returning data from all sensors as json
+
+    # endpoint returning data from all sensors as json
     @app.get("/data/all")
     async def root():
-        #list that is supposed to contain tuples with (mac, minimum timestamp, max timestamp)
+        # list that is supposed to contain tuples with (mac, minimum timestamp, max timestamp)
         macsAndTimestamps = []
 
-        #prints mac_addr, min and max timestamps as well as appends them to list
+        # prints mac_addr, min and max timestamps as well as appends them to list
         for row in mqttClient.getUniqueMacAddresses():
             print(row)
             macsAndTimestamps.append(row)
 
-        #loops that print full data rows for each sensor
+        # loops that print full data rows for each sensor
         for mac in macsAndTimestamps:
-            #mac[0] is mac address from tuple mentioned in fist comment in this method
+            # mac[0] is mac address from tuple mentioned in fist comment in this method
             for mac_row in mqttClient.getAllDataForSensor(mac[0]):
                 print(mac_row)
-            #delimiter between each distinct mac address
+            # delimiter between each distinct mac address
             print("===")
 
-        #response list
+        # response list
         ret_data = []
 
-        #appending data to return list in format specified in our system
+        # appending data to return list in format specified in our system
         for mac in macsAndTimestamps:
-            #first append min, max datetime for each sensor, assign timezone offset and specify sub-sensor Id
-            tmp_dict = {'datetime': {'from': mac[1], 'to': mac[2], 'timezone_offset': 7200}, 'sensor': {"mac_address": mac[0]}}
+            # first append min, max datetime for each sensor, assign timezone offset and specify sub-sensor Id
+            tmp_dict = {'datetime': {'from': mac[1], 'to': mac[2], 'timezone_offset': 7200},
+                        'sensor': {"mac_address": mac[0]}}
             data = []
             for measurementType in types:
-                #gathering distinct ids of sub-sensors cuz only one cursor can be opened at once
+                # gathering distinct ids of sub-sensors cuz only one cursor can be opened at once
                 ids = []
                 for idTuple in mqttClient.getSubSensorsIdsByMacAddressAndType(mac[0], measurementType):
                     ids.append(idTuple[0])
-                #appending data from each sub-sensor with given type to return json
+                # appending data from each sub-sensor with given type to return json
                 for id in ids:
                     dataForType = {'name': measurementType, 'id': id}
                     measurement_list = []
@@ -84,9 +88,11 @@ if __name__ == "__main__":
         ret = mqttClient.getAllDataTest()
         data = []
         for row in ret:
-            data.append(MQTTData(log_id=row[0], timestamp=row[1], mac_addr=row[2], type=row[3], sensor_id=row[4], reading=row[5]))
+            data.append(MQTTData(log_id=row[0], timestamp=row[1], mac_addr=row[2], type=row[3], sensor_id=row[4],
+                                 reading=row[5]))
 
         return data
+
 
     @app.get("/data/temp", response_model=List[MQTTData])
     async def root():
@@ -97,5 +103,6 @@ if __name__ == "__main__":
                                  reading=row[5]))
 
         return data
+
 
     uvicorn.run(app, host="127.0.0.1", port=8000)
