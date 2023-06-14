bdecd3cbb05c50b176133d42b6108928ad449c3c
MRajczyk <mikolajrajczyk01@gmail.com>
2023-06-14 02:30:23 +0200

    implemented creating sqlite db in home directory appropriate for user platform

--- greensense_backend/DbConnection.py
+++ greensense_backend/DbConnection.py
@@ -1,9 +1,25 @@
 import sqlite3
+import os
+import platform
 
 
 class DbConnection:
     def __init__(self, database):
-        self.conn = con = sqlite3.connect(database, check_same_thread=False)
+        directory = ".greensense"
+        home_path = os.path.expanduser("~")
+
+        new_dir_path = home_path
+        if platform.system() == "Linux":
+            new_dir_path = os.path.join(home_path, ".local", directory)
+        elif platform.system() == "Windows":
+            new_dir_path = os.path.join(home_path, "AppData", "Roaming", directory)
+        else:
+            new_dir_path = os.path.join(home_path, directory)  #such case will exist???? :O
+
+        if not os.path.exists(new_dir_path):
+            os.mkdir(new_dir_path, mode=0o777)
+
+        self.conn = con = sqlite3.connect(os.path.join(new_dir_path, database), check_same_thread=False)
         self.cur = con.cursor()
         self.cur.execute("create table if not exists mqtt_logs (log_id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp INTEGER NOT NULL, mac_addr TEXT NOT NULL, type TEXT, sensor_id INTEGER, reading TEXT NOT NULL)")
 
